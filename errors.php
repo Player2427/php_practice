9c490846f207253d2911ff64e9ce2650d3bc6071
ivan <ivanmarinin2002@gmail.com>
2023-03-16 16:16:22 +0300

    First commit

---
+++ errors.php
@@ -0,0 +1,22 @@
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+echo "Ошибки";
+
+// function func() {
+//     func();
+// };
+// func();
+
+// include_once ("index.php");
+
+// echo $a;
+
+// $a = split(',', 'a,b');
+
+class foo {
+    function foo() {
+        echo 'Я конструктор!';
+    }
+}
